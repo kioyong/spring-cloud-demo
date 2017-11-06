083622a58e3ed59a1b8c48c29d247e47729be7d6
LiangYong <yong.a.liang@accenture.com>
2017-11-06 18:31:17 +0800

    update properties

--- yong-configServer/src/main/java/com/yong/config/yongconfigServer/YongConfigServerApplication.java
+++ yong-configServer/src/main/java/com/yong/config/yongconfigServer/YongConfigServerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableConfigServer
-@EnableEurekaClient
 public class YongConfigServerApplication {
 
 	public static void main(String[] args) {
